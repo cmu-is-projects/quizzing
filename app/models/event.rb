b210e878905ba07711882e0eb170e658abba47f4
Theophilus Onime <tonime@andrew.cmu.edu>
2015-02-23 01:42:44 -0500

    finished writing code and tests for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,9 +5,11 @@ class Event < ActiveRecord::Base
 
   #Validations
   # validates_presence_of :start_date, :end_date, :start_time, :num_rounds
-  # validates_date :start_date
+  # validates_time :start_time, :end_time
+  # validates_date :start_date, :on_or_after => lambda { Date.today }, :on_or_after_message => "cannot be in the past", on:  :create
   # validates_date :end_date, :on_or_after => :start_date, :on_or_after_message => "cannot end before beginning"
   # validates_numericality_of :num_rounds, only_integer: true, greater_than: 0
+
 
   # #Scopes
   # scope :chronological, -> {order("start_date", "end_date")}
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,21 +4,37 @@ class EventTest < ActiveSupport::TestCase
 
   #VALIDATONS
   should have_many(:quizzes)
+  should validate_presence_of(:event_id)
   should validate_presence_of(:start_date)
   should validate_presence_of(:start_time)
   should validate_presence_of(:num_rounds)
-
+  # START DATE
   should allow_value(Date.today).for(:start_date)
   should allow_value(1.day.from_now.to_date).for(:start_date)
   should_not allow_value(1.day.ago.to_date).for(:start_date)
   should_not allow_value("bad").for(:start_date)
   should_not allow_value(2).for(:start_date)
   should_not allow_value(3.14159).for(:start_date)
-  
+  #END_DATE  
   should_not allow_value("bad").for(:end_date)
   should_not allow_value(2).for(:end_date)
   should_not allow_value(3.14159).for(:end_date) 
-
+  #START_TIME
+  should allow_value(Time.now).for(:start_time)
+  should_not allow_value("am").for(:start_time)
+  should_not allow_value("pm").for(:start_time)
+  should_not allow_value("bad").for(:start_time)
+  should_not allow_value("1:00").for(:start_time)  
+  should_not allow_value(900).for(:start_time)
+  #END_TIME
+  should allow_value(nil).for(:end_time)
+  should allow_value(2.hours.from_now).for(:end_time)
+  should_not allow_value("am").for(:end_time)
+  should_not allow_value("pm").for(:end_time)
+  should_not allow_value("bad").for(:end_time)
+  should_not allow_value("1:00").for(:end_time)  
+  should_not allow_value(900).for(:end_time)
+  #NUM_ROUNDS
   should validate_numericality_of(:num_rounds)
   #unsure of a realistic cap for num_rounds
   should allow_value(120).for(:num_rounds)
@@ -27,20 +43,5 @@ class EventTest < ActiveSupport::TestCase
   should_not allow_value(-20).for(:num_rounds)
   should_not allow_value(3.14159).for(:num_rounds)
 
-  should allow_value("am").for(:time_slot)
-  should allow_value("pm").for(:time_slot)
-  should_not allow_value("bad").for(:time_slot)
-  should_not allow_value("1:00").for(:time_slot)  
-  should_not allow_value(900).for(:time_slot)
-
-  
-  should validate_numericality_of(:max_students)
-  should allow_value(nil).for(:max_students)
-  should allow_value(1).for(:max_students)
-  should allow_value(12).for(:max_students)
-  should_not allow_value("bad").for(:max_students)
-  should_not allow_value(0).for(:max_students)
-  should_not allow_value(-1).for(:max_students)
-  should_not allow_value(3.14159).for(:max_students)
 
 end
