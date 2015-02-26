2ca9e38a653ae6f346ffccebfdbbb72216bdd730
Nathan Oh <njo@andrew.cmu.edu>
2015-02-25 22:11:25 -0500

    started writing coach tests

--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class CoachTest < ActiveSupport::TestCase
-  # test relationships
+  # validations
   should belong_to(:organization)
   should have_many(:team_coaches)
   should have_many(:teams).through(:team_coaches)
   should belong_to(:user)
+  
   should accept_nested_attributes_for(:user).allow_destroy(true)
+  should validate_presence_of(:user_id)
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+
+
 end
