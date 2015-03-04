a79c7f4e6f3ac65be67840f4d3de0de0b373756e
Theophilus Onime <tonime@andrew.cmu.edu>
2015-03-03 23:03:56 -0500

    added organization_id to events safely

--- test/contexts.rb
+++ test/contexts.rb
@@ -6,3 +6,16 @@ require 'quiz_contexts'
 require 'team_contexts'
 require 'coach_contexts'
 require 'event_contexts'
+
+# module Contexts
+#   # explicitly include all sets of contexts used for testing 
+#   include Contexts::CurriculumContexts
+#   include Contexts::InstructorContexts
+#   include Contexts::CampContexts
+#   include Contexts::CampInstructorContexts
+#   include Contexts::LocationContexts
+#   include Contexts::FamilyContexts
+#   include Contexts::StudentContexts
+#   include Contexts::RegistrationContexts
+#   include Contexts::UserContexts
+# end
