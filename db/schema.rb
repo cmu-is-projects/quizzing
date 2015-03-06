afee743b2d32b4d228aa5a0c5603dda50c572fa6
Theophilus Onime <tonime@andrew.cmu.edu>
2015-03-06 11:27:55 -0500

    added active to divisions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304050031) do
+ActiveRecord::Schema.define(version: 20150306162027) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150304050031) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
   create_table "events", force: true do |t|
