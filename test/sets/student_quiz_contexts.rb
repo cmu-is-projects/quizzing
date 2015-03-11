48f40ff237740c21b1616d996e093afe344901b1
profh <profh@cmu.edu>
2015-03-11 12:09:20 -0400

    Modified student quiz context so scores not in descending order

--- test/sets/student_quiz_contexts.rb
+++ test/sets/student_quiz_contexts.rb
@@ -3,14 +3,6 @@ module Contexts
 
     # assumes create_students, create_one_event (and related) already run
     def create_student_quizzes
-      # student with perfect day
-      @mark1 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz1, num_correct: 4, num_attempts: 4, num_fouls: 0)
-      @mark2 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz2, num_correct: 4, num_attempts: 4, num_fouls: 0)
-      @mark3 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz3, num_correct: 4, num_attempts: 4, num_fouls: 0)
-      @mark4 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz4, num_correct: 4, num_attempts: 4, num_fouls: 0)
-      @mark5 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz5, num_correct: 4, num_attempts: 4, num_fouls: 0)
-      @mark6 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz6, num_correct: 4, num_attempts: 4, num_fouls: 0)
-
       # student with average day 
       @alex1 = FactoryGirl.create(:student_quiz, student: @alex, quiz: @quiz1, num_correct: 4, num_attempts: 4, num_fouls: 0)
       @alex2 = FactoryGirl.create(:student_quiz, student: @alex, quiz: @quiz2, num_correct: 4, num_attempts: 5, num_fouls: 0)
@@ -18,7 +10,15 @@ module Contexts
       @alex4 = FactoryGirl.create(:student_quiz, student: @alex, quiz: @quiz4, num_correct: 2, num_attempts: 5, num_fouls: 0)
       @alex5 = FactoryGirl.create(:student_quiz, student: @alex, quiz: @quiz5, num_correct: 3, num_attempts: 3, num_fouls: 0)
       @alex6 = FactoryGirl.create(:student_quiz, student: @alex, quiz: @quiz6, num_correct: 0, num_attempts: 1, num_fouls: 0)
-   
+
+      # student with perfect day
+      @mark1 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz1, num_correct: 4, num_attempts: 4, num_fouls: 0)
+      @mark2 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz2, num_correct: 4, num_attempts: 4, num_fouls: 0)
+      @mark3 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz3, num_correct: 4, num_attempts: 4, num_fouls: 0)
+      @mark4 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz4, num_correct: 4, num_attempts: 4, num_fouls: 0)
+      @mark5 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz5, num_correct: 4, num_attempts: 4, num_fouls: 0)
+      @mark6 = FactoryGirl.create(:student_quiz, student: @mark, quiz: @quiz6, num_correct: 4, num_attempts: 4, num_fouls: 0)   
+
       # student with negative day 
       @jimmy1 = FactoryGirl.create(:student_quiz, student: @jimmy, quiz: @quiz1, num_correct: 1, num_attempts: 4, num_fouls: 0)
       @jimmy2 = FactoryGirl.create(:student_quiz, student: @jimmy, quiz: @quiz2, num_correct: 0, num_attempts: 0, num_fouls: 1)
