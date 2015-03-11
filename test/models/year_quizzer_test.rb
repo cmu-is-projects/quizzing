3ab8247f10fc657e5d1d5f96b9786993ad90a9dd
profh <profh@cmu.edu>
2015-03-11 10:18:19 -0400

    Very basic framework for year_quizzer test set up

---
+++ test/models/year_quizzer_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class YearQuizzerTest < ActiveSupport::TestCase
+  context "Creating a context" do
+    setup do
+      create_one_event
+      create_one_division
+      # build_context
+    end
+
+    teardown do
+      delete_one_event
+      delete_one_division
+      # destroy_context
+    end
+
+    
+
+  end
+end
