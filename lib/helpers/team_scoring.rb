5d00896c627c5a2842406d4a2afe960e4c2e2caa
Lan Betancourt <ibetanco@andrew.cmu.edu>
2015-03-16 20:37:40 -0400

    Minor changes to team scoring logic

--- lib/helpers/team_scoring.rb
+++ lib/helpers/team_scoring.rb
@@ -10,9 +10,15 @@ module QuizHelpers
       return errors < 3 ? (errors - 2) * 10 : 0
     end
 
+    # This method will be used to determine the total scoring for an entire
+    # team. A team must be passed in, so we can differentiate students from
+    # the other team. This method will also take into account total quizzer
+    # errors for the final score, using the previous method.
     def calculate_team_score(student_quizzes_array, team)
       team_score = 0
       num_participants = 0
+      total_num_correct = 0
+      total_num_attempts = 0
 
       #Loop through the array of all the students involved in the quiz
       student_quizzes_array.each{|sqi|
@@ -22,11 +28,21 @@ module QuizHelpers
           team_score += sqi.score
           #Keep tabs on 3rd, 4th, 5th person bonus
           num_participants += sqi.num_correct > 0 ? 1 : 0
+
+          #We will use this information at the end when we calculate team error
+          total_num_correct += sqi.num_correct
+          total_num_attempts += sqi.num_attempts
         end
       }
 
       #Apply 3rd, 4th, 5th person bonus if applicable
       team_score += num_participants >= 3 ? team_score += 10 : 0
+      
+      #Now we take into account the total errors
+      err = calculate_team_error_points(total_num_correct, total_num_attempts)
+      team_score += err
+
+      return team_score
     end
 
   end #module TeamScoring
