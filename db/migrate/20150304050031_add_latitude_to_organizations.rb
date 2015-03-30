7f93e82b95354bb18d95f553399c4acf50200233
Theophilus Onime <tonime@andrew.cmu.edu>
2015-03-30 17:57:04 -0400

    updated gitignore and rm'ed migrations

--- db/migrate/20150304050031_add_latitude_to_organizations.rb
+++
@@ -1,5 +0,0 @@
-class AddLatitudeToOrganizations < ActiveRecord::Migration
-  def change
-    add_column :organizations, :latitude, :decimal
-  end
-end
--- db/migrate/20150306161444_add_active_to_divisions.rb
+++
@@ -1,5 +0,0 @@
-class AddActiveToDivisions < ActiveRecord::Migration
-  def change
-    add_column :divisions, :active, :boolean
-  end
-end
