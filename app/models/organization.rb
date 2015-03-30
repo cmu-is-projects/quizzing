a12eafb727e76f2e85f82533f3bec1aee9c3b637
Theophilus Onime <tonime@andrew.cmu.edu>
2015-03-30 01:51:54 -0400

    passing all organization except Canadian zip

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -21,12 +21,12 @@ class Organization < ActiveRecord::Base
   validates :zip, presence: true, unless: "street_1.nil?"
   #case 2 - if zip entered, need street 1 to geocode
   validates :street_1, presence: true, unless: "zip.nil?"
-  # #case 3 - if state is entered, if a zip is entered with it, the zip needs to be 5 digits 
-  # #and continue to discern the allow_blank...
-  # validates :zip, format: { with: /\A\d{5}\z/, message: "should be five digits long because an American state was provided", allow_blank: true }, if: :zip_and_state_entered?
-  # #case 4 - if zip is entered without a state, the zip should then be of Canadian format
-  # #fiddling with method taken from online
-  # #validates_as_postal_code :zip, country: "CA", allow_blank: true, message: "The zip you entered should be of Canadian format because you did not enter a US state", if: :zip_is_entered_without_a_state?
+  #case 3 - if state is entered - if a zip is entered with it - the zip needs to be 5 digits 
+  #and continue to discern the allow_blank...
+  validates :zip, format: { with: /\A\d{5}\z/, message: "should be five digits long because an American state was provided", allow_blank: true }, if: :zip_and_state_entered?
+  #case 4 - if zip is entered without a state, the zip should then be of Canadian format
+  #fiddling with method taken from online
+  #validates_as_postal_code :zip, country: "CA", allow_blank: true, message: "The zip you entered should be of Canadian format because you did not enter a US state", if: :zip_is_entered_without_a_state?
   # validates :zip, format: { with: /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/, message: "The zip you entered should be of Canadian format because you did not enter a US state", allow_blank: true }, if: :zip_is_entered_without_a_state?
 
   
@@ -51,11 +51,11 @@ class Organization < ActiveRecord::Base
   private
   #TODO - test
   def zip_and_state_entered?
-    return ( !zip.nil? && !state.nil? )
+    return ( !self.zip.nil? && !self.state.nil? )
   end
 
   def zip_is_entered_without_a_state?
-    return ( !zip.nil? && state.nil? )
+    return ( !self.zip.nil? && self.state.nil? )
   end
 
   def end_student_tenure_if_organization_made_inactive
