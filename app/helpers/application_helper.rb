3324312ab6b294980cad94126328e6a64f2c9f1a
Nathan Oh <njo@andrew.cmu.edu>
2015-04-01 14:17:21 -0400

    modify time field in event form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def options_for_time_select
     start_time = Array.new
     for $h in 6..18 do
-        for $m in ['00', '15', '30', '45'] do
+        for $m in ['00', '30'] do
             start_time.push [$h.to_s + ":" + $m.to_s, "%02d" % $h + ":" + $m + ":00"]
         end
     end
