c33b63d8beecbc4b91c94db1c40dbcecb806ce9e
Nathan Oh <njo@andrew.cmu.edu>
2015-04-11 12:18:36 -0400

    trying ajax for active and inactive partial on coach homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,13 @@ class HomeController < ApplicationController
   	@events = Event.all 
     @upcoming_events = Event.upcoming.chronological.to_a
     @active_teams = Team.all.active
-    @inactive_teams = Team.all.inactive
+    @inactive_students = Student.all.inactive
+  end
+
+  def display
+    respond_to do |format|
+      format.js { render partial: '_inactive_roster' }
+    end
   end
 
   def about
