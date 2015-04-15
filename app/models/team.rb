0ed77e4c295e0e42eda0f985e942fda98b13c697
Nathan Oh <njo@andrew.cmu.edu>
2015-04-15 11:40:21 -0400

    problems when empty db

--- app/models/team.rb
+++ app/models/team.rb
@@ -34,6 +34,7 @@
   # Methods
 
   # Returns active teams that are not at capacity
+  # TODO: error if the db is empty
   def self.not_at_capacity(organization=nil, division=nil)
     tmp = Array.new
     if organization && division #if organization and division are provided
