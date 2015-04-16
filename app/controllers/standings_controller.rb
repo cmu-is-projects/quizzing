126358f1473b1fbb05c928cf9ca64ea1b612adce
Theophilus Onime <tonime@andrew.cmu.edu>
2015-04-16 14:42:27 -0400

    closed some open list item tags in the nav and have year_team WRITTEN according to year_quizzer

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -5,8 +5,8 @@ class StandingsController < ApplicationController
   		division_id = get_default_division_id
   	else
   		division_id = params[:id]
-  		
   	end
+    
   	@division = Division.find(division_id)
   	#TODO (probably NOT during CMU 67-373 Spring 2015): adjust this to ultimately take a QuizYear parameter
   	@quizzers = YearQuizzer.get_all_quizzers_for_division_for_year(@division)
