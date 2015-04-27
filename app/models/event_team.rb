1338f1bba69f2ec2bf85096d84bd7da5d8708a0c
Theophilus Onime <tonime@andrew.cmu.edu>
2015-04-27 00:44:39 -0400

    event details standings works

--- app/models/event_team.rb
+++ app/models/event_team.rb
@@ -4,10 +4,11 @@ class EventTeam
     @name = team.name
     @event = event
     @students = team.students
+    @division = team.division
     @quiz_teams = get_all_quiz_teams_for_team_in_this_event
   end
 
-  attr_reader :event, :team, :students, :name, :quiz_teams
+  attr_reader :event, :team, :students, :name, :quiz_teams, :division
 
   def total_score
     #Top down; go to QuizTeam
@@ -19,25 +20,25 @@ class EventTeam
   end
 
   #Necessary for event details page
-  #   def self.get_all_quizzers_for_event(event)
-  #   all_students_at_event = StudentQuiz.for_event(event).map(&:student_id).uniq
-  #   quizzers = Array.new
-  #   all_students_at_event.each do |stu_id|
-  #     event_quizzer = EventQuizzer.new(Student.find(stu_id), event)
-  #     quizzers << event_quizzer
-  #   end
-  #   # returned a sorted array of event_quizzers in descending order of average points
-  #   # (until end when avg and total align, averages rule the day in reporting results)
-  #   sorted = quizzers.sort_by{|eq| eq.average_points}.reverse
-  # end
+    def self.get_all_teams_for_event(event)
+    all_teams_at_event = QuizTeam.for_event(event).map(&:team_id).uniq
+    teams = Array.new
+    all_teams_at_event.each do |team_id|
+      event_team = EventTeam.new(Team.find(team_id), event)
+      teams << event_team
+    end
+    # returned a sorted array of event_teams in descending order of average points
+    # (until end when avg and total align, averages rule the day in reporting results)
+    sorted = teams.sort_by{|et| et.total_score}.reverse
+  end
 
-  # def self.get_all_quizzers_for_event_and_division(event, division)
-  #   event_quizzers = EventQuizzer.get_all_quizzers_for_event(event)
-  #   in_division = Array.new
-  #   event_quizzers.each do |event_quizzer|
-  #     in_division << event_quizzer if event_quizzer.division == division
-  #   end
-  #   # resort just to be safe...
-  #   final = in_division.sort_by{|eq| eq.average_points}.reverse 
-  # end
+  def self.get_all_teams_for_event_and_division(event, division)
+    event_teams = EventTeam.get_all_teams_for_event(event)
+    in_division = Array.new
+    event_teams.each do |event_team|
+      in_division << event_team if event_team.division == division
+    end
+    # resort just to be safe...
+    final = in_division.sort_by{|et| et.total_score}.reverse 
+  end
 end #class EventTeam
