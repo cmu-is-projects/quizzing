c5bb605166221cda33072002b557dd9a2d9ff689
Nick Gasbarro <ngasbarr@andrew.cmu.edu>
2015-05-26 14:45:17 -0400

    Intermediate step, finished standing models

--- app/models/indiv_standing.rb
+++ app/models/indiv_standing.rb
@@ -1,7 +1,14 @@
 class IndivStanding < ActiveRecord::Base
-	# Validations
-	validates_numericality_of :position, only_integer: true, greater_than: 0, less_than: 100
-	validates_numericality_of :student_id, only_integer: true, greater_than: 0
-	validates_numericality_of :current_team, only_integer: true, greater_than: 0
-	validates_numericality_of :total_points, only_integer: true, greater_than: 0
+        # Validations
+
+        validates_presence_of :position, :student_id, :current_team, :total_points, :lowest_score, :adjusted_points, :accuracy
+
+        validates_numericality_of :position, only_integer: true, greater_than: 0, less_than: 100
+        validates_numericality_of :student_id, only_integer: true, greater_than: 0
+        validates_numericality_of :current_team, only_integer: true, greater_than: 0
+        validates_numericality_of :total_points, only_integer: true, greater_than: 0
+        validates_numericality_of :lowest_score, only_integer: true, greater_than: 0
+        validates_numericality_of :adjusted_points, only_integer: true, greater_than: 0
+        validates_numericality_of :accuracy, less_than: 1.01
+
 end
--- app/models/team_standing.rb
+++ app/models/team_standing.rb
@@ -1,2 +1,17 @@
 class TeamStanding < ActiveRecord::Base
+
+        # Relationships
+        belongs_to :teams
+
+
+        validates_presence_of :position, :team_id, :total_points, :accuracy
+
+        validates_numericality_of :position, only_integer: true, greater_than: 0, less_than: 100
+        validates_numericality_of :team_id, only_integer: true, greater_than: 0
+        validates_numericality_of :total_points, only_integer: true, greater_than: 0
+        validates_numericality_of :accuracy, less_than: 1.01
+
+        # Scopes
+        scope :for_division, -> { joins(:teams).where('teams.division_id = ?', 1) }
+        #scope :for_division, -> { includes(teams:[:division] )}
 end
