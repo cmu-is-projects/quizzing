e88a8aa494021e746f06b900112a72934ceec447
Nick Gasbarro <ngasbarr@andrew.cmu.edu>
2015-05-27 11:45:13 -0400

    Added by_position scope

--- app/models/indiv_standing.rb
+++ app/models/indiv_standing.rb
@@ -12,6 +12,7 @@ class IndivStanding < ActiveRecord::Base
         validates_numericality_of :accuracy, less_than: 1.01
 
         # Scopes
+        scope :by_position, -> { order(:position) }
         scope :for_juniors, -> { where('division_id = ?', "#{Division.find_by_name('juniors').id}") }
         scope :for_seniors, -> { where('division_id = ?', "#{Division.find_by_name('seniors').id}") }
         scope :for_seniorb, -> { where('division_id = ?', "#{Division.find_by_name('seniorb').id}") }
