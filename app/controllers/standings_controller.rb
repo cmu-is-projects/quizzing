2abf26209faa1c3a37e14593589885aa6460d25b
Nick Gasbarro <ngasbarr@andrew.cmu.edu>
2015-06-01 13:15:20 -0400

    Indiv and team standings index pages

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -1,12 +1,12 @@
 class StandingsController < ApplicationController
 	def index
-		@team_juniors = TeamStanding.all.for_juniors
-		@indiv_juniors = IndivStanding.all.for_juniors
+		@team_juniors = TeamStanding.all.for_juniors[0..4]
+		@indiv_juniors = IndivStanding.all.for_juniors[0..4]
 
-		@team_seniors = TeamStanding.all.for_seniors
-		@indiv_seniors = IndivStanding.all.for_seniors
+		@team_seniors = TeamStanding.all.for_seniors[0..4]
+		@indiv_seniors = IndivStanding.all.for_seniors[0..4]
 
-		@team_seniorb = TeamStanding.all.for_seniorb
-		@indiv_seniorb = IndivStanding.all.for_seniorb
+		@team_seniorb = TeamStanding.all.for_seniorb[0..4]
+		@indiv_seniorb = IndivStanding.all.for_seniorb[0..4]
 	end
 end
