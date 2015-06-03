b4546ca4e832c6f97d2d3a0508922958c5e1647e
Clara Shim <seohyuns@andrew.cmu.edu>
2015-06-02 22:50:51 -0400

    changed the graph to line graph now data needs to be added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -30,16 +30,17 @@ class StudentsController < ApplicationController
     @accuracy_percentage = (@year_quizzer.total_accuracy*100.0).round(1)
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => "Performance")
-      f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
-      f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
-      f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
+      f.xAxis(:categories => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+      f.series(:name => "Event1", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
+      f.series(:name => "Event2", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
 
       f.yAxis [
-        {:title => {:text => "GDP in Billions", :margin => 70} },
+        {:title => {:text => "Quiz Scores", :margin => 70} },
         {:title => {:text => "Population in Millions"}, :opposite => true},
       ]
 
-      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
+
       f.chart({:defaultSeriesType=>"line"})
     end 
   end
