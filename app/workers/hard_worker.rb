eec158b1885c4f9fd7454576ebaf677df831a64e
Nick Gasbarro <ngasbarr@andrew.cmu.edu>
2015-06-08 11:44:44 -0400

    Sidekiq fully working, added options to update individual/team separately

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,15 @@
+require_relative '../../lib/database_switcher'
 class HardWorker
-  include Sidekiq::Worker
+	include DatabaseSwitcher
+	#connect_to_db('pgh')
+	include Sidekiq::Worker
 
   def perform(area)
-  	Rake::Task['db:update_area_standings'].invoke(area)
+  	#Rake::Task['db:update_area_standings'].invoke(area)
+  	#puts "#{Student.first.student_quizzes.count}"
+  	#Rake::Task['test_task']
+
+  	#TODO configure to pass in subdomain from session
+  	system "rake db:update_area_standings[#{area}]"
   end
 end
