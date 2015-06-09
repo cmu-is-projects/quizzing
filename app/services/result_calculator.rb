812358dae1acd2c4b234a87e618b9f49da619db4
profh <profh@cmu.edu>
2015-06-09 13:24:18 -0400

    Added method for most improved

--- app/services/result_calculator.rb
+++ app/services/result_calculator.rb
@@ -1,5 +1,18 @@
 module ResultCalculator
   def self.find_most_improved(organization, division)
-    students = organization.current_students.select{|s|}
+    students = organization.current_students_for_division(division)
+    events = Event.past.chronological
+    return Array.new if events.size < 2
+    last_event = events.pop
+    previous_event = events.pop
+    improved = Array.new
+    students.each do |student|
+      last_result = EventQuizzer.new(student, last_event)
+      previous_result = EventQuizzer.new(student, previous_event)
+      diff = last_result.total_points - previous_result.total_points
+      tmp = [student.proper_name,last_result.total_points, diff, last_result.accuracy]
+      improved << tmp
+    end
+    return improved
   end
 end
