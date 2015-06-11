deb722d9715c8666a02d6ef8f29e3b0c1b4ee029
Audrey Alpizar <audrey.alpizar@gmail.com>
2015-06-11 13:14:00 -0400

    coach dashboard - top students expand and collapse

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -67,3 +67,19 @@ $(document).ready(function(){
     });
 
 
+$(document).ready(function() {
+    $('tr.more-top-students').hide();
+    $('a.more-top-students').click(function(event) {
+        event.preventDefault();
+        $('tr.more-top-students').fadeToggle('fast', function() {
+          if( $('tr.more-top-students').is(':visible') ) {
+           $('a.more-top-students').html('Less <i class="fa fa-angle-double-up"></i>')
+          }
+          else {
+           $('a.more-top-students').html('More <i class="fa fa-angle-double-down"></i>')
+          }           
+        });
+        
+    });
+  });
+
